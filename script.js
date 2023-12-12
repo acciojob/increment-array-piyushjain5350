d1baa5312266224356804abce1fda0c84b3d2eb7
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-12 22:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
   //your code here
+	for(let i=0;i<arr.length;i++){
+		arr[i]=arr[i]+1;
+	}
+	return arr;
 }
 
 alert(incrementArray(arr));
